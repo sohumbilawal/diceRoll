58381be69e946e6588e9e54003de7549ea399e0a
sohumbilawal <sohumbilawal@gmail.com>
2018-01-31 11:30:56 -0800

    Added defensive dicerolls
    Added sections to calculate defensive dice rolls, and their first and second highest dice face numbers.

--- diceRoll.py
+++ diceRoll.py
@@ -11,8 +11,9 @@ numADice = 3
 numDDice = 2
 
 aDice = []
+dDice = []
 
-def diceRoll(num):
+def diceRollA(num):
     i = 0
     while (i < num):
         die = randint(1,6)
@@ -20,6 +21,15 @@ def diceRoll(num):
         i += 1
     #aDice.extend([max(aDice)])
     return aDice
+
+def diceRollD(num):
+    i = 0
+    while (i < num):
+        die = randint(1,6)
+        dDice.extend([die])
+        i += 1
+    #aDice.extend([max(aDice)])
+    return dDice
 
 #aDice = [d1, d2, d3]
 
@@ -34,17 +44,46 @@ def diceRoll(num):
 #d3 = aDice[2]
 
 if numADice == 3:
-    print("Rolling 3 dice: ", diceRoll(3))
+    print("Rolling 3 dice: ", diceRollA(3))
+
+    highA = heapq.nlargest(2, aDice[0:len(aDice)])
+    maxA = highA[0]
+    max2A = highA[1]
+    print("The highest attacker roll is: {0}, and second highest is: {1}".format(maxA, max2A))
+
 elif numADice == 2:
-    print("Rolling 2 dice: ", diceRoll(2))
+    print("Rolling 2 dice: ", diceRollA(2))
+
+    highA = heapq.nlargest(2, aDice[0:len(aDice)])
+    maxA = highA[0]
+    max2A = highA[1]
+    print("The highest attacker roll is: {0}, and second highest is: {1}".format(maxA, max2A))
+
 elif numADice == 1:
-    print("Rolling 1 die: ", diceRoll(1))
+    print("Rolling 1 die: ", diceRollA(1))
+
+    maxA = max(aDice)
+    print("The highest attacker roll is: {0}".format(maxA))
+
+if numDDice == 2:
+    print("Rolling 2 dice: ", diceRollD(2))
+
+    highD = heapq.nlargest(2, dDice[0:len(dDice)])
+    maxD = highD[0]
+    max2D = highD[1]
+    print("The highest attacker roll is: {0}, and second highest is: {1}".format(maxD, max2D))
+
+elif numDDice == 1:
+    print("Rolling 1 die: ", diceRollD(1))
+
+    maxD = max(dDice)
+    print("The highest attacker roll is: {0}".format(maxD))
 
 #aHeap = heapq.heapify(aDice)
-highA = heapq.nlargest(2, aDice[0:len(aDice)])
-maxA = highA[0]
-max2A = highA[1]
-print("The highest attacker roll is: {0}, and second highest is: {1}".format(maxA, max2A))
+#highA = heapq.nlargest(2, aDice[0:len(aDice)])
+#maxA = highA[0]
+#max2A = highA[1]
+#print("The highest attacker roll is: {0}, and second highest is: {1}".format(maxA, max2A))
 #print(aDice)
 
 #print(aDice)
